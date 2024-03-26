0ac292efd956ff0ab3d93095aa55cc8090b27ae4
bongcoy <mpascaldewantara@gmail.com>
2024-03-26 15:23:56 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,4 @@
+// File Objective: save notes object in array
+const notes = [];
+
+module.exports = notes;
---
+++ src/server.js
@@ -0,0 +1,26 @@
+// File Objective: configure and run HTTP server using Hapi
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    // cors is activated on all routes on the server
+    routes: {
+      // allows data to be consumed by all origins
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // server routes
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
